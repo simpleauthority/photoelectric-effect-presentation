54bc6f39bb8bb52685a460a3653152ac98e813a0
Jacob Andersen <jacob@algorithmjunkie.com>
2022-03-02 10:15:30 -0800

    fix path again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/photoelectric-effect/",
+  base: "/physics-presentations/photoelectric-effect/",
   plugins: [vue()],
   resolve: {
     alias: {
